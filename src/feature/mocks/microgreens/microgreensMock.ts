c4266ff5dc171e6aafed23cb9c1bb7cf31d87342
Alex Andru <alex007d@gmail.com>
2023-11-17 00:33:35 +0100

    Refactor mocks to be standalone variables

--- src/feature/mocks/microgreens/microgreensMock.ts
+++ src/feature/mocks/microgreens/microgreensMock.ts
@@ -1,28 +1,32 @@
 import { type MicrogreenStructure } from "../../types";
 
-export const microgreens: MicrogreenStructure[] = [
-  {
-    name: "Radish Micros",
-    approx_yield: 280,
-    blackout_time_days: 3,
-    daily_light_integral: 12,
-    germination_time_days: 3,
-    harvest_time_days: 10,
+export const radishMicrosMock: MicrogreenStructure = {
+  name: "Radish Micros",
+  approx_yield: 280,
+  blackout_time_days: 3,
+  daily_light_integral: 12,
+  germination_time_days: 3,
+  harvest_time_days: 10,
 
-    seed_quantity: 28,
-    seed_type: "Radish Seeds",
-    total_light_days: 4,
-  },
-  {
-    name: "Broccoli Microgreens",
-    approx_yield: 280,
-    blackout_time_days: 3,
-    daily_light_integral: 12,
-    germination_time_days: 2,
-    harvest_time_days: 10,
+  seed_quantity: 28,
+  seed_type: "Radish Seeds",
+  total_light_days: 4,
+};
 
-    seed_quantity: 28,
-    seed_type: "Broccoli Seeds",
-    total_light_days: 5,
-  },
+export const brocoliMicrosMock: MicrogreenStructure = {
+  name: "Broccoli Microgreens",
+  approx_yield: 280,
+  blackout_time_days: 3,
+  daily_light_integral: 12,
+  germination_time_days: 2,
+  harvest_time_days: 10,
+
+  seed_quantity: 28,
+  seed_type: "Broccoli Seeds",
+  total_light_days: 5,
+};
+
+export const microgreensMock: MicrogreenStructure[] = [
+  radishMicrosMock,
+  brocoliMicrosMock,
 ];
