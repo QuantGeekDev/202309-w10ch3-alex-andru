8eab53d775c4fb519fe077091632c444c0bfdfa8
Alex Andru <alex007d@gmail.com>
2023-11-16 22:41:06 +0100

    Add MicrogreensController

--- src/feature/microgreens/controller/MicrogreensController.ts
+++ src/feature/microgreens/controller/MicrogreensController.ts
@@ -1,10 +1,25 @@
 import { type Request, type Response } from "express";
+import { type MicrogreensRepository } from "../../types";
 
 class MicrogreensController {
-  public getMicrogreens(req: Request, res: Response) {
-    const microgreens = ["Test", "Test2"];
-    res.status(200).json(microgreens);
-  }
+  constructor(public microgreensRepository: MicrogreensRepository) {}
+
+  public getMicrogreens = async (
+    _req: Request,
+    res: Response,
+  ): Promise<void> => {
+    try {
+      const microgreens = await this.microgreensRepository
+        .getMicrogreens()
+        .then();
+      res.status(200).json(microgreens);
+    } catch (error) {
+      console.error("Error getting microgreens from DB:", error);
+      res
+        .status(501)
+        .json({ error: "Error getting Microgreens from Database" });
+    }
+  };
 }
 
 export default MicrogreensController;
--- src/feature/microgreens/controller/repository/MicrogreensRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from "express";
-import { type Microgreen } from "../../../types";
-
-class MicrogreensRepository {
-  public getMicrogreens(): Microgreen[] {
-    return [];
-  }
-}
