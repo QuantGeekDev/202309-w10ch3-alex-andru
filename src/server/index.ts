80cfd4c5ba965db77ee3f0d3f76697ae4023114c
Alex Andru <alex007d@gmail.com>
2023-11-16 22:33:20 +0100

    Add middlewares to server index

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,8 +3,11 @@ import app from "./app.js";
 import pingRouter from "../feature/ping/pingRouter.js";
 import resourceNotFound from "./middlewares/errors/resourceNotFound.js";
 import microgreensRouter from "../feature/microgreens/microgreensRouter.js";
+import express from "express";
 
-app.use(morgan("dev"));
 app.get("/", pingRouter);
 app.use("/microgreens", microgreensRouter);
+
+app.use(morgan("dev"));
+app.use(express.json());
 app.use(resourceNotFound);
