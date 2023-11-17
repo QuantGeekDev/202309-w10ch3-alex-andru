78b9ee5723a21a93dde11349e2ac0c85c4874233
Alex Andru <alex007d@gmail.com>
2023-11-17 13:28:26 +0100

    Add /add route to microgreensRouter

--- src/feature/microgreens/microgreensRouter.ts
+++ src/feature/microgreens/microgreensRouter.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import MicrogreensController from "./controller/MicrogreensController.js";
 import MicrogreensMongooseRepository from "./repository/MicrogreensMongooseRepository.js";
+
 const microgreensRouter = Router();
+
 const microgreensRepository = new MicrogreensMongooseRepository();
 const microgreensController = new MicrogreensController(microgreensRepository);
 
 microgreensRouter.get("/", microgreensController.getMicrogreens);
 microgreensRouter.get("/:id", microgreensController.getMicrogreenById);
+microgreensRouter.post("/add", microgreensController.addMicrogreen);
 
 export default microgreensRouter;
